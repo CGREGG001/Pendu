74ab2c85ad26594df4e580d6d3a524d222a83904
Gregory C <cgregg@belgacom.net>
2024-01-25 12:51:08 +0100

    base programme

--- main.c
+++ main.c
@@ -3,6 +3,23 @@
 
 int main(int argc, char const *argv[])
 {
+    int nombresEssais = 10;
+    char motMystere[] = "MARRON", lettreTrouvee[6] = {0}, lettre = 0;
+
+    printf("Bienvenue dnas le jeu du pendu\n");
+    printf("Vous avez %d essais\n", nombresEssais);
+
+    while (nombresEssais > 0)
+    {
+        printf("Quel est le mot mystere ? ");
+
+        
+        
+
+
+        nombresEssais --;
+    }
     
+    printf("Fin du jeu\n");
     return 0;
 }
